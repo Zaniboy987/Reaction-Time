8e32c1ba84bb21b4aaef825efaeb1bd42aa96ab4
Zaniboy987 <nazarazan13@gmail.com>
2022-07-18 15:56:28 -0400

    added menu() and leaderboard()

--- reactionTime.java
+++ reactionTime.java
@@ -10,9 +10,39 @@ import static java.lang.System.currentTimeMillis;
 public class reactionTime {
     // opening sequence
     static void opening() {
-        System.out.println("WELCOME TO REACTION TIME!\n\n" +
+        System.out.println("********************" +
+                "WELCOME TO REACTION TIME!\n\n" +
                 "You will be challenged on how fast you can react!\n" +
                 "*************************************************\n");
+    }
+
+    // recurring menu
+    static void menu() {
+        // Initialize variables
+        Scanner input = new Scanner(System.in);
+        int num;
+
+        // do-while loop to determine action of the user
+        do {
+            System.out.println("1. PLAY!\t2. LEADERBOARD\t3. EXIT\n\n" +
+                    "Enter the number for your desired ACTION: ");
+            num = input.nextInt();
+        } while(num != 1 || num != 2 || num != 3);
+
+    }
+
+    // leaderboards
+    static void leaderboard(int tries, ArrayList<Long> times) {
+        System.out.println("RANK\tTIME");
+
+        // loops for the amount of tries by the player
+        for (int i = 0; i < tries; i++) {
+            System.out.println(i + "\t" + times.get(i));
+        }
+
+        // setting to return to main menu
+        System.out.println("******************************************" +
+                "Enter 1 to return to the main menu\n\n");
     }
 
     // main method
@@ -20,7 +50,8 @@ public class reactionTime {
         // initialize variables
         int tries = 0;
 
-        // utilizing methods
         opening();
+        menu();
+
     }
 }
