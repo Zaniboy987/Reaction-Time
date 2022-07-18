7fbbce70e109c6f05194b309936f30d81018b99c
Zaniboy987 <nazarazan13@gmail.com>
2022-07-18 13:56:43 -0400

    added reactionTime.java

---
+++ reactionTime.java
@@ -0,0 +1,26 @@
+package main;
+// imported libraries
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import static java.lang.System.currentTimeMillis;
+
+// main class
+public class reactionTime {
+    // opening sequence
+    static void opening() {
+        System.out.println("WELCOME TO REACTION TIME!\n\n" +
+                "You will be challenged on how fast you can react!\n" +
+                "*************************************************\n");
+    }
+
+    // main method
+    public static void main(String[] args) {
+        // initialize variables
+        int tries = 0;
+
+        // utilizing methods
+        opening();
+    }
+}
