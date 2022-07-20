feb12e39df8275ad12c784b32eaeceb7f656efed
Zaniboy987 <nazarazan13@gmail.com>
2022-07-20 16:12:02 -0400

    Changed interface available options

--- reactionTime.java
+++ reactionTime.java
@@ -10,25 +10,35 @@ import static java.lang.System.currentTimeMillis;
 public class reactionTime {
     // opening sequence
     static void opening() {
-        System.out.println("********************" +
-                "WELCOME TO REACTION TIME!\n\n" +
+        System.out.println("**************************************************\n" +
+                "\t\t\tWELCOME TO REACTION TIME!\n" +
+                "**************************************************\n" +
                 "You will be challenged on how fast you can react!\n" +
-                "*************************************************\n");
+                "**************************************************\n");
     }
 
     // recurring menu
     static void menu() {
         // Initialize variables
         Scanner input = new Scanner(System.in);
-        int num;
+        int num = 0;
 
         // do-while loop to determine action of the user
         do {
             System.out.println("1. PLAY!\t2. LEADERBOARD\t3. EXIT\n\n" +
-                    "Enter the number for your desired ACTION: ");
+                    "Enter the number for your desired ACTION!");
             num = input.nextInt();
-        } while(num != 1 || num != 2 || num != 3);
+        } while (num > 3);
 
+        // switch cases determine next actions of the user
+        switch (num) {
+            case 1:
+                System.out.println("You chose the option to PLAY");
+                break;
+            case 2:
+                System.out.println("You chose the option to view the leaderboard");
+                break;
+        }
     }
 
     // leaderboards
@@ -41,8 +51,13 @@ public class reactionTime {
         }
 
         // setting to return to main menu
-        System.out.println("******************************************" +
+        System.out.println("**************************************************\n" +
                 "Enter 1 to return to the main menu\n\n");
+    }
+
+
+    static long game() {
+        return 0;
     }
 
     // main method
@@ -52,6 +67,12 @@ public class reactionTime {
 
         opening();
         menu();
+        // leaderboard();
+
+        // ending screen
+        System.out.println("\n**************************************************\n" +
+                "THANK YOU FOR PLAYING!!!\n" +
+                "**************************************************");
 
     }
 }
